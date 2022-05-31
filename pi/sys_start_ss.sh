5e0087c64b95fc72e8b6e6b2b12d7207937e5f5d
FalconRay <falconray@yahoo.com>
2022-05-31 09:31:47 +0800

    Make sure dns and ss-redir are up before change to new port.

--- pi/sys_start_ss.sh
+++ pi/sys_start_ss.sh
@@ -1,5 +1,32 @@
 #!/bin/ash
 
+set +e
+
+TBONE=""
+while [ -z ${TBONE} ]
+do
+    #echo "TBONE is empty, looping..."
+    TBONE="$(nslookup tbone.synology.me | grep 'Address 1:' | awk '{print substr($0, 12)}')"
+    sleep 1
+done
+
+uci get shadowsocks-libev.ss_rules.dst_ips_bypass
+if [ $? -eq 0 ]
+then
+    uci delete shadowsocks-libev.ss_rules.dst_ips_bypass
+    uci delete shadowsocks-libev.ss_rules.src_ips_bypass
+    uci commit
+fi
+
+uci add_list shadowsocks-libev.ss_rules.dst_ips_bypass=${TBONE}
+uci add_list shadowsocks-libev.ss_rules.src_ips_bypass=${TBONE}
+uci set shadowsocks-libev.ss_rules.dst_default='forward'
+uci set shadowsocks-libev.ss_rules.src_default='checkdst'
+uci commit
+echo "TBONE:${TBONE}"
+
+#####################################################################################################################################
+
 SS_PORTS='8000 9000 9001 5222 5223 5228 8080 8801 8802 8410 14000 16285'
 first_port=8000
 last_port=16285
@@ -36,30 +63,5 @@ uci set shadowsocks-libev.@server[0].server_port=${next_port}
 uci commit
 
 
-
-TBONE=""
-while [ -z ${TBONE} ]
-do
-#echo "TBONE is empty, looping..."
-TBONE="$(nslookup tbone.synology.me | grep 'Address 1:' | awk '{print substr($0, 12)}')"
-sleep 1
-done
-
-set +e
-uci get shadowsocks-libev.ss_rules.dst_ips_bypass
-if [ $? -eq 0 ]
-then
-uci delete shadowsocks-libev.ss_rules.dst_ips_bypass
-uci delete shadowsocks-libev.ss_rules.src_ips_bypass
-uci commit
-fi
-set -e
-
-uci add_list shadowsocks-libev.ss_rules.dst_ips_bypass=${TBONE}
-uci add_list shadowsocks-libev.ss_rules.src_ips_bypass=${TBONE}
-uci set shadowsocks-libev.ss_rules.dst_default='forward'
-uci set shadowsocks-libev.ss_rules.src_default='checkdst'
-uci commit
-echo "TBONE:${TBONE}"
 exit 0
 
